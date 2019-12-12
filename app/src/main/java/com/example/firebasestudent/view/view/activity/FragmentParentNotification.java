61ceb4e7eeb959e52a33adea46e9815d7380adaf
LongDinh <longdcnb1998@gmail.com>
2019-12-12 17:24:43 +0700

    master

---
+++ app/src/main/java/com/example/firebasestudent/view/view/activity/FragmentParentNotification.java
@@ -0,0 +1,75 @@
+package com.example.firebasestudent.view.view.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebasestudent.R;
+import com.example.firebasestudent.databinding.FragmentParentNotificationBinding;
+import com.example.firebasestudent.model.Notice;
+import com.example.firebasestudent.view.view.adapter.NotificationAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class FragmentParentNotification extends Fragment {
+
+    FragmentParentNotificationBinding binding;
+
+    private ArrayList<Notice> notices;
+    private NotificationAdapter adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       binding = DataBindingUtil.inflate(inflater, R.layout.fragment_parent_notification,container,false);
+
+
+        String studentName = getArguments().getString("StudentName");
+        String className =  getArguments().getString("ClassName");
+        String positionClass =  getArguments().getString("PositionClass");
+        String position =  getArguments().getString("Position");
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference NotilRef = firebaseDatabase.getReference().child("school").child("classes").child(positionClass).child("notice");
+        NotilRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("HocSinh",dataSnapshot.toString());
+                notices = new ArrayList<>();
+                for (DataSnapshot snapshot:dataSnapshot.getChildren()) {
+                    Notice notice = snapshot.getValue(Notice.class);
+                    notices.add(notice);
+                }
+                adapter = new NotificationAdapter(getContext(),notices);
+                binding.recyclerViewStudent.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL,false));
+                binding.recyclerViewStudent.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        binding.textViewStudentName.setText(studentName);
+        binding.textViewClassName.setText(className);
+
+       View view = binding.getRoot();
+
+       return view;
+    }
+}
--- app/src/main/java/com/example/firebasestudent/view/view/activity/ParentActivity.java
+++ app/src/main/java/com/example/firebasestudent/view/view/activity/ParentActivity.java
@@ -3,11 +3,13 @@ package com.example.firebasestudent.view.view.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.firebasestudent.R;
@@ -15,8 +17,11 @@ import com.example.firebasestudent.databinding.ActivityParentBinding;
 import com.example.firebasestudent.model.Notice;
 import com.example.firebasestudent.view.view.adapter.NotificationAdapter;
 import com.example.firebasestudent.view.view.adapter.ViewPagerAdapter;
+import com.example.firebasestudent.view.view.fragment.FragmentMessage;
+import com.example.firebasestudent.view.view.fragment.FragmentParentTuitionFee;
 import com.example.firebasestudent.view.view.fragment.Fragment_NotificationGeneral;
 import com.example.firebasestudent.view.view.fragment.Fragment_NotificationPrivate;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,11 +34,13 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParentActivity extends AppCompatActivity {
+public class ParentActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     ActivityParentBinding binding ;
     private NotificationAdapter adapter;
     private ArrayList<Notice> notices;
+    private FragmentParentNotification notification;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +50,12 @@ public class ParentActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_parent);
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this);
 
         String studentName = this.getIntent().getStringExtra("StudentName");
         String className = this.getIntent().getStringExtra("ClassName");
         String positionClass = this.getIntent().getStringExtra("PositionClass");
         String position = this.getIntent().getStringExtra("Position");
-
 
 //        Log.d("LongDinh",jsonNoticeGen + jsonNoticePri);
 //
@@ -93,5 +100,49 @@ public class ParentActivity extends AppCompatActivity {
 //                finish();
 //            }
 //        });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("ClassName",className);
+        bundle.putString("Position",position);
+        bundle.putString("PositionClass",positionClass);
+        bundle.putString("StudentName",studentName);
+
+        notification = new FragmentParentNotification();
+        notification.setArguments(bundle);
+
+        loadFragment(notification);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.Fragment_container_parent,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_notification_parent:
+                fragment = notification;
+                break;
+            case R.id.navigation_HocPhi:
+                fragment = new FragmentParentTuitionFee();
+                break;
+            case R.id.navigation_mess:
+                fragment = new FragmentMessage();
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
